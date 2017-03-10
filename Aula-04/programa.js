4000910b29fd2b45f9eddbbd32b65e4267fd3f4d
DANIEL PRATA SAVOY <daniel.psavoy@CAS411234MAC25.local>
2017-03-10 10:35:04 -0300

    js

--- Aula-04/programa.js
+++ Aula-04/programa.js
@@ -1,5 +1,16 @@
+var lista = [
+  "japao",
+  "franca"
+];
+var contador = 0;
+
 function mudarBandeira(){
-  document.body.className = "japao";
+  document.body.className = lista[contador];
+  if(contador < lista.length-1){
+    contador = contador + 1;
+  } else {
+    contador = 0;
+  }
 }
 
 window.addEventListener("click", mudarBandeira);
