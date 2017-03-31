d6f1c1e6be29cc5c38d56ec9c84e4a69f1c95cbb
DANIEL PRATA SAVOY <daniel.psavoy@CAS411234MAC25.local>
2017-03-31 08:42:43 -0300

    js

---
+++ Aula-07/particulas.js
@@ -0,0 +1,14 @@
+class Particula{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.desenhar();
+  }
+
+  desenhar = function(){
+    var eu = document.createElement("div");
+    eu.style.left = this.x + "px";
+    eu.style.bottom = this.y + "px";
+    document.body.appendChild(eu);
+  }
+}
