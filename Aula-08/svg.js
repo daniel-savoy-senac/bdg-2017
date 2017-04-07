4027e4c0ee69cc6abd54f56e597b1615101f1488
DANIEL PRATA SAVOY <daniel.psavoy@CAS411234MAC25.local>
2017-04-07 09:38:27 -0300

    points

--- Aula-08/svg.js
+++ Aula-08/svg.js
@@ -1,12 +1,33 @@
+var poly, atual;
+
+function iniciar(){
+  poly = document.querySelector("polyline");
+  window.addEventListener("mousemove", mudar);
+  animar();
+}
+
 function mudar(evt){
-  var poly = document.querySelector("polyline");
   var x = evt.x;
   var y = evt.y;
   var txt = x+","+y;
-  var atual = poly.getAttribute("points");
-  atual = atual +" "+txt;
+  atual = poly.getAttribute("points");
+  atual = atual ? atual.split(" ") : [];
+  atual.push(txt);
+  atual = atual.join(" ");
   poly.setAttribute("points", atual);
 }
 
+function animar(){
+  atual = poly.getAttribute("points");
+  if(!atual) return window.requestAnimationFrame(animar);;
+  var points = atual
+    .split(" ")
+    .map(a=>a.split(",")
+      .map(b=>Number(b)+Math.random()*4-2)
+      .join(",")
+    ).join(" ");
+  poly.setAttribute("points", points);
+  window.requestAnimationFrame(animar);
+}
 
-window.addEventListener("click", mudar);
+window.addEventListener("load", iniciar);
