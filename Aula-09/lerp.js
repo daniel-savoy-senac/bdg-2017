bb92eea16daeeb34d185eeba5d81e6d819b2db61
DANIEL PRATA SAVOY <daniel.psavoy@CAS411234MAC25.local>
2017-05-05 09:00:53 -0300

    js

--- Aula-09/lerp.js
+++ Aula-09/lerp.js
@@ -5,9 +5,16 @@ function tempo(x,y){
   return x/w;
 }
 
-function calcularTempo(e){
+function animar(e){
   t = tempo(e.x, e.y);
-  console.log(t);
+  // movendo de (10,200) até (400,100)
+  let px = lerp(10,400,t);
+  let py = lerp(200,100,t);
+  console.log(`X:${px}, Y${px}`);
 }
 
-window.addEventListener("mousemove", calcularTempo)
+function lerp(a,b,t){
+  return (1-t) * a + t * b;
+}
+
+window.addEventListener("mousemove", animar)
