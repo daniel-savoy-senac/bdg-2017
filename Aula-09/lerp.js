248663d071bceff78a1ef344185039d8a42f07ec
DANIEL PRATA SAVOY <daniel.psavoy@CAS411234MAC25.local>
2017-05-05 10:44:18 -0300

    clamp

--- Aula-09/lerp.js
+++ Aula-09/lerp.js
@@ -26,6 +26,12 @@ function animar(e){
   bloco1.style.left = px + "px";
   bloco1.style.top = py + "px";
   bloco1.style.transform = `rotate(${angulo}deg)`;
+
+  let t2 = cena(.5, .8, t);
+  console.log(t);
+  console.log(t2);
+  let cor = lerp(0,100,t2);
+  bloco2.style.background = `hsl(240,100%,${cor}%)`;
 }
 
 function lerp(a,b,t){
@@ -35,6 +41,7 @@ function lerp(a,b,t){
 function clamp(t){
   if(t>1) return 1;
   if(t<0) return 0;
+  return t;
 }
 
 window.addEventListener("load", iniciar);
